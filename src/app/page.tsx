48ca9f4171991fad18f30d6585d13d2349d3c7dc
pdas9647 <padmanabhadas9647@gmail.com>
2025-04-20 02:20:02 +0530

    removed getSession()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import {getSession} from "@/app/actions";
 import Logout from "@/components/Logout";
 
 async function Home() {
-    const session = await getSession();
+    // const session = await getSession();
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
@@ -12,7 +11,8 @@ async function Home() {
                 <div>
                     <Image
                         className="rounded-lg"
-                        src={session?.user?.image || "https://images.pexels.com/photos/1374510/pexels-photo-1374510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
+                        // src={session?.user?.image || "https://images.pexels.com/photos/1374510/pexels-photo-1374510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
+                        src={"https://images.pexels.com/photos/1374510/pexels-photo-1374510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
                         alt="img"
                         width={500}
                         height={500}
@@ -20,7 +20,8 @@ async function Home() {
                     />
                 </div>
                 <div>
-                    <h1 className="mb-2 text-2xl font-bold">Welcome, {session?.user.name}!</h1>
+                    {/*<h1 className="mb-2 text-2xl font-bold">Welcome, {session?.user.name}!</h1>*/}
+                    <h1 className="mb-2 text-2xl font-bold">Welcome!</h1>
                     <p className="text-muted-foreground">
                         If you are learning something valuable from this video, please like
                         and subscribe to my channel.
